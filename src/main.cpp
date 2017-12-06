432014dfe5aad97900300c21a85a080bb56a961e
xd009642 <danielmckenna93@gmail.com>
2017-12-06 20:33:49 +0000

    Test ADC build

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,8 @@ LCD_DISCO_F469NI lcd;
 TS_DISCO_F469NI ts;
 SDFileSystem sd("sd");
 Timer t;
+InterruptIn wire(PA_1);
+InterruptIn wire2(PG_13);
 
 static constexpr uint32_t BACKGROUND_COLOUR = 0xFFED0000;
 static constexpr uint32_t SCREEN_WIDTH = 800;
@@ -21,6 +23,9 @@ static constexpr uint32_t SCREEN_HEIGHT = 480;
 // These are approximates just to give decent spacing
 static constexpr uint32_t BORDER_HEIGHT = 60;
 static constexpr uint32_t BORDER_WIDTH = 120;
+
+volatile bool triggered = false;
+volatile bool triggered2 = false;
 bool init_sd_card() {
     return sd.disk_initialize() == 0;
 }
@@ -41,10 +46,22 @@ void launch_pong()
     lcd.DisplayStringAt(0, LINE(10), (uint8_t*)"PLAYING PONG", RIGHT_MODE);
 }
 
+void trig()
+{
+    triggered = true;
+}
+
+void trig2()
+{
+    triggered2 = true;
+}
+
 int main()
 {
     t.start();
     TS_StateTypeDef touch;
+    wire.rise(&trig);
+    wire2.rise(&trig2);
     uint8_t text[30];
     uint8_t status;
     lcd.SetTextColor(LCD_COLOR_WHITE);
@@ -54,8 +71,23 @@ int main()
     status = ts.Init(lcd.GetXSize(), lcd.GetYSize());
     prepare_background();
     // 0 means good, non-zero is error code. 
-    lcd.DisplayStringAt(0, LINE(5), (uint8_t *)"XMAS CHALLENGE", CENTER_MODE);
-  
+    lcd.DisplayStringAt(0, LINE(2), (uint8_t *)"XMAS CHALLENGE", CENTER_MODE);
+    auto a0 = AnalogIn(A0);
+    auto a1 = AnalogIn(A1);
+    auto a2 = AnalogIn(A2);
+    auto a3 = AnalogIn(A3);
+    while(1)
+    {
+        sprintf((char*)text, "A0 = %f", a0.read());
+        lcd.DisplayStringAt(0, LINE(3), (uint8_t*)text, LEFT_MODE);
+        sprintf((char*)text, "A1 = %f", a1.read());
+        lcd.DisplayStringAt(0, LINE(4), (uint8_t*)text, LEFT_MODE);
+        sprintf((char*)text, "A2 = %f", a2.read());
+        lcd.DisplayStringAt(0, LINE(5), (uint8_t*)text, LEFT_MODE);
+        sprintf((char*)text, "A3 = %f", a3.read());
+        lcd.DisplayStringAt(0, LINE(6), (uint8_t*)text, LEFT_MODE);
+        wait_ms(16);
+    }
     render::framebuffer buffer(26, 26);
     ds::ring outer(buffer, ds::outer); 
     if(buffer.is_valid() == false)
@@ -95,7 +127,14 @@ int main()
         int bytes = 23*sizeof(uint32_t);
         //int bytes = np::render_segment(np::INNER_0, buffer.get_render_column(col), 26);
         t.stop();
-        sprintf((char*)text, "Rendered %d bytes in %fs", bytes, t.read());
+        if(!triggered)
+            sprintf((char*)text, "Rendered %d bytes in %fs", bytes, t.read());
+        else 
+            sprintf((char*)text, "I'm triggered");
+        if(triggered2) {
+            uint8_t text2[] = "hello";
+            lcd.DisplayStringAt(0, LINE(14), text2, LEFT_MODE);
+        }
         t.reset();
         lcd.DisplayStringAt(0, LINE(15), text, LEFT_MODE);
         buffer.pixel_at(col,col)+=20;
