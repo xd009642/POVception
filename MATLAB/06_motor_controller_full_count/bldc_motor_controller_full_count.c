3a2d0b6fde6ff15c52eaa3c53967ceacd8a88405
cmmc4 <M.Cox-09@student.lboro.ac.uk>
2017-12-10 16:21:03 +0000

    fixed motor halt state fixed motor halt state

--- MATLAB/06_motor_controller_full_count/bldc_motor_controller_full_count.c
+++ MATLAB/06_motor_controller_full_count/bldc_motor_controller_full_count.c
@@ -3,9 +3,9 @@
  *
  * Code generated for Simulink model 'bldc_motor_controller_full_count'.
  *
- * Model version                  : 1.81
+ * Model version                  : 1.84
  * Simulink Coder version         : 8.12 (R2017a) 16-Feb-2017
- * C/C++ source code generated on : Sun Dec 10 16:08:08 2017
+ * C/C++ source code generated on : Sun Dec 10 16:20:21 2017
  *
  * Target selection: ert.tlc
  * Embedded hardware selection: ARM Compatible->ARM Cortex
